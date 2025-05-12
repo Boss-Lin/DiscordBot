40b0b901440d84a9e422460da4ac0fe2c437d941
Boss-Lin <59110277+Boss-Lin@users.noreply.github.com>
2025-05-12 17:53:04 +0800

    Update load_env.py

--- load_env.py
+++ load_env.py
@@ -3,6 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# MTM3MTM3NTM0NTYwMDU2MTI2NA.GqIDZw.vc69RLQ2LDVkJGOJuQHohZXbWo-qoKOTPVnPAo
 def get_discord_token():
     return os.getenv("DISCORD_TOKEN")
